c16d2b6e2a7510beab54d2fe8cb2772c54da3ad8
huuthe1999 <huuthe128@gmail.com>
2023-03-15 13:25:50 +0700

    chore: :rotating_light: add editor config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
